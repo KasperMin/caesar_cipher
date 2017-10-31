14aa43c3d78bafa68880ede6bbf298410edbffc3
KasperMin <kaspermsimonsen@gmail.comgit config --global user.email git config --global user.email kaspermsimonsen@gmail.com>
2017-10-31 19:51:47 +0100

    corrections made

--- substrings.rb
+++ substrings.rb
@@ -1,23 +1,28 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings word_or_sentence, dictionary
-  string_array = word_or_sentence.split(' ');
-  regexp_array = [];
+  string_array = word_or_sentence.split(' ')
+  clean_array = []
+  matches = []
   occurences = Hash.new 0
 
   string_array.each do |element|
-    downcase_lement_minus_nonAlpha_char = element.downcase.gsub(/\W[^.]*/, '');
-    word_to_regexp = Regexp.new(downcase_lement_minus_nonAlpha_char)
-    regexp_array.push(word_to_regexp)
+    clean_array.push(element.gsub(/\W[^.]*/, '');
+    clean_array.push(downcase_char)
+  end
+  clean_array = clean_array.join("")
+
+  dictionary.each do |dictionary_word|
+    regexp_word = Regexp.new(dictionary_word)
+    clean_array.scan(regexp_word) {|match| matches << match}
   end
 
-  regexp_array.each do |regexp_word|
-    matches = dictionary.join().scan regexp_word
-    matches.each do |word|
-      occurences[word] +=1
-    end
+  matches.each do |word|
+    occurences[word] +=1
   end
+
   occurences
+
 end
 
 
